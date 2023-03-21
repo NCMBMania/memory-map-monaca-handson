e3a1331da9ceb780e46aefa1c0be65ebd9d05092
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2023-03-21 11:14:28 +0900

    NCMBのコードを削除

--- www/js/app.js
+++ www/js/app.js
@@ -6,12 +6,14 @@ document.addEventListener(event, async (e) => {
   const config = await (await fetch('./js/config.json')).json();
   // NCMBの初期化
   window.ncmb = new NCMB(config.applicationKey, config.clientKey);
+  /*
   // ログイン状態を確認
   const user = ncmb.User.getCurrentUser();
   // ログインしていない場合は匿名ログイン
   if (!user) {
     await ncmb.User.loginAsAnonymous();
   }
+  */
   window.app = new Framework7({
     name: 'My App', // App name
     theme: 'auto', // Automatic theme detection
